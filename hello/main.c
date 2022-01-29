640767569fb8a53f2bf5756e4f07b22616a4cade
徐晓飞 <xuxiaofei.666@bytedance.com>
2022-01-29 14:43:45 +0800

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Bytedance on 2022/1/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
